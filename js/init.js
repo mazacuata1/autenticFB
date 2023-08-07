c25a6da36c3147fcbc37ff3ec0f359c9b0a1740a
mazacuata1 <yankee1234511@gmail.com>
2023-08-07 17:42:58 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyD9VTpK9xHj1utBxXcK7gFTBMsG34Yx_tI",
+  authDomain: "authenticatormazacuata.firebaseapp.com",
+  projectId: "authenticatormazacuata",
+  storageBucket: "authenticatormazacuata.appspot.com",
+  messagingSenderId: "708833574907",
+  appId: "1:708833574907:web:bfe7bcfb80eaf17736155e",
+  measurementId: "G-W9X74ZE2M9"
+    });
